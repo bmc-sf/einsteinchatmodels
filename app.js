56229536247313a57c0883821ede7bcff8e74b6c
bmc-sf <29308937+bmc-sf@users.noreply.github.com>
2024-10-14 20:13:01 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const http = require('http');
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const hostname = '127.0.0.1';
 const port = 3000;
@@ -7,13 +7,21 @@ const port = 3000;
 const app = express();
 
 app.use(express.static('public'));
+app.use(bodyParser.json()); // Middleware to parse JSON body
 
 app.get('/', (req, res) => {
-    //res.send('Hello There!');
-    res.send(index.html);
-   });
+    res.sendFile(__dirname + '/public/index.html'); // Serve the HTML file
+});
 
+app.post('/submit', (req, res) => {
+    const userPrompt = req.body.prompt; // Get the prompt from the request body
+    let myMsg = 'You clicked Submit: ' + userPrompt;
+    console.log(myMsg);
+
+    // Respond with a JSON object
+    res.json({ response: myMsg });
+});
 
 app.listen(port, () => {
     console.log(`Server is running at http://${hostname}:${port}`);
-   });
+});
